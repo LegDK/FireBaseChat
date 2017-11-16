99fa07d07e05ecfc7b906346ea5c09588b614a31
LegDK <maltsev.ru95@mail.ru>
2017-11-16 16:42:21 +0500

    Initial commit

---
+++ app/src/main/java/legdk/ru/firebasechat/UI/MainActivity.java
@@ -0,0 +1,15 @@
+package legdk.ru.firebasechat.UI;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import legdk.ru.firebasechat.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
